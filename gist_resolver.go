c55850c849c591e9cee93a580d3e1dfa51519b6b
Miguel Osorio @Kelvur <Osorio10.92@gmail.com>
2020-06-04 18:46:25 +0200

    docs: improve the comments/documentation of gist resolver

--- gist_resolver.go
+++ gist_resolver.go
@@ -9,31 +9,34 @@ import (
 	"regexp"
 )
 
-// GistResolver is a
+// GistResolver is able to understand gist locators and fetch
+// them returning a stream
 //
 // • It implements the infertace Resolver
 type GistResolver struct {
 }
 
-// GistFileResponse struct
-type GistFileResponse struct {
+// UserGistFile represents a gist file in a Gisthub response
+type UserGistFile struct {
 	Filename string
 	Raw_URL  string
 }
 
-// GistResponse struct
-type GistResponse struct {
+// UserGistsResponse represents the response of Github when
+// fetching all the gist of a user
+type UserGistsResponse struct {
 	ID    string
-	Files map[string]GistFileResponse
+	Files map[string]UserGistFile
 }
 
 // GithubAPIURL constant reference the base url of Github API
 var GithubAPIURL string = "https://api.github.com"
 
-// GistLocatorRegexp regexp to validate and match the useful parts of the gist locator
+// GistLocatorRegexp regexp to validate and match the parts of the gist locator
 var GistLocatorRegexp string = `^[a-zA-Z0-9]+://(?P<username>[a-zA-Z0-9_]+[a-zA-Z0-9])/(?P<resource>[^/<>|:&]+)$`
 
-// ObtainUsernameAndResourceFromGistLocator function
+// ObtainUsernameAndResourceFromGistLocator return an array
+// with the username and resource of a gist locator
 func ObtainUsernameAndResourceFromGistLocator(locator string) ([2]string, error) {
 	re := regexp.MustCompile(GistLocatorRegexp)
 	if !re.MatchString(locator) {
@@ -43,12 +46,14 @@ func ObtainUsernameAndResourceFromGistLocator(locator string) ([2]string, error)
 	return [2]string{matchList[1], matchList[2]}, nil
 }
 
-// GetUserGistURL function
+// GetUserGistURL builds a URL which points to all the gist of
+// a user
 func GetUserGistURL(username string) string {
 	return fmt.Sprintf("%s/users/%s/gists", GithubAPIURL, username)
 }
 
-// FetchData function
+// FetchData function fetch the code where the resource Locator
+// points to, in this case the resource points to a gist
 func (resolver *GistResolver) FetchData(resource *Resource) (io.ReadCloser, error) {
 	values, err := ObtainUsernameAndResourceFromGistLocator(resource.Locator)
 	if err != nil {
@@ -64,12 +69,13 @@ func (resolver *GistResolver) FetchData(resource *Resource) (io.ReadCloser, erro
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	// Decode the response
-	var gistList []GistResponse
+	var gistList []UserGistsResponse
+
 	if err = json.Unmarshal(body, &gistList); err != nil {
 		return nil, fmt.Errorf("Unable to decode the request to %s %v", url, err)
 	}
 	// Look for the resource in all the gist
-	var found GistFileResponse = GistFileResponse{"", ""}
+	var found UserGistFile = UserGistFile{"", ""}
 	for idxGist := range gistList {
 		for filename, fileList := range gistList[idxGist].Files {
 			// Check if the filename is equals to the resource ol the locator
